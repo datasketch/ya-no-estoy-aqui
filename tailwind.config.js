89c8383bb5a1fe02cb08a7cfcb836e080d067b93
David Daza <03dazal@gmail.com>
2020-11-10 14:40:24 -0500

    estructura para móviles

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'sizzling-red': '#ef5d60',
+        'wild-blue-yonder': '#a8aace',
+        'silver-chalice': '#b2b2b2',
+      },
+      fontFamily: {
+        lato: ['Lato', 'sans-serif'],
+        charter: ['Charter', 'sans-serif'],
+        playfair: ['"Playfair Display"', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
