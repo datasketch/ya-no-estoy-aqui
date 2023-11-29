691a91658000940f2a1ce6a243b580412181bfa9
David Daza <03dazal@gmail.com>
2023-11-29 17:01:52 -0500

    Build from static JSON

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import slugify from 'slugify';
+import data from '../data/victims.json'
 
 async function getData() {
   try {
-    const response = await axios.get('https://script.google.com/macros/s/AKfycbywq9AJvYXrR4d2IVY9NFM3F2QBzGZN2_LPdAU8Y4hUfmiU8jJvm72XVO-YREDlilt7/exec')
-    const { data } = response.data
-    const victims = data.dataviz
-      .filter((victim) => victim.visible === 1)
+    // const response = await axios.get('https://script.google.com/macros/s/AKfycbywq9AJvYXrR4d2IVY9NFM3F2QBzGZN2_LPdAU8Y4hUfmiU8jJvm72XVO-YREDlilt7/exec')
+    const victims = data
+      .filter((victim) => victim.visible === 1 && victim['Nombre'] && victim['Apellidos'])
       .map((victim) => ({
         fullname: `${victim['Nombre']} ${victim['Apellidos']}`.trim(),
         slug: slugify(`${victim['Nombre']} ${victim['Apellidos']}`, {
